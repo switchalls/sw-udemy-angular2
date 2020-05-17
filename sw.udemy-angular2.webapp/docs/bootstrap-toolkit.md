9b7de4e0dc028d160251ac3c9074dce29a8165ed
stewart.witchalls <stewart.witchalls@cdl.co.uk>
2020-05-17 12:10:32 +0100

    Step 12. Outputting a List of Recipes

--- sw.udemy-angular2.webapp/docs/bootstrap-toolkit.md
+++ sw.udemy-angular2.webapp/docs/bootstrap-toolkit.md
@@ -5,7 +5,7 @@ View [getting started](https://getbootstrap.com/docs/4.0/getting-started/introdu
 ## Widgets
 
 * [Grid System](https://getbootstrap.com/docs/4.0/layout/grid/)
-
+  
   eg. `shopping-list.component.html`
   
   ```
@@ -13,10 +13,26 @@ View [getting started](https://getbootstrap.com/docs/4.0/getting-started/introdu
       <div class="col-md-10">
   ```
 
+* [List group](https://getbootstrap.com/docs/4.0/components/list-group/)
+  
+  eg.
+  
+  ```
+  <a href="#" class="list-group-item clearfix">
+      <div class="pull-left">
+        <h4 class="list-group-item-heading">Title</h4>
+        <p class="list=group-item-text">Description</p>
+      </div>
+      <span class="pull-right">
+        <img src="http://recipe.com/cake.jpg" class="img-responsive" style="max-height: 50px" />
+      </span>
+  </a>
+  ```
+
 * [Navbar](https://getbootstrap.com/docs/4.0/components/navbar/)
-
+  
   eg. `app-header.component.html`
-
+  
   ```
   <nav class="navbar navbar-default">
   ```
--- sw.udemy-angular2.webapp/src/main/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ sw.udemy-angular2.webapp/src/main/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from "../recipe.model";
+
 @Component({
   selector: 'app-recipe-list',
-  template: `
-    <p>
-      recipe-list works!
-    </p>
-  `,
+  templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+    recipes: Recipe[] = [
+        new Recipe("Vanilla Cake", "A classic vanilla cake recipe made completely from scratch!", "https://i2.wp.com/www.sugarspunrun.com/wp-content/uploads/2018/01/Vanilla-Cake-Recipe-1-of-1-15.jpg"),
+        new Recipe("Beckys Butter Cake", "Butter cake recipe reminiscent of the 1-2-3-4 cake that Grandma may have baked", "https://storcpdkenticomedia.blob.core.windows.net/media/recipemanagementsystem/media/recipe-media-files/recipes/retail/x17/16730-beckys-butter-cake-600x600.jpg?ext=.jpg"),
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
